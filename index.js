992f7b0a3b5dab5ebe77f96323d194fc00256acc
Piyush-Keshri-1704 <98079268+Piyush-Keshri-1704@users.noreply.github.com>
2022-03-20 12:20:13 +0530

    updated

--- index.js
+++ index.js
@@ -23,15 +23,7 @@ $(document).keypress(function() {
   }
 });
 
-$(document).click(function() {
-  if (!started) {
 
-  
-    $("#level-title").text("Level " + level);
-    nextSequence();
-    started = true;
-  }
-});
 
 $(".btn").click(function() {
 
